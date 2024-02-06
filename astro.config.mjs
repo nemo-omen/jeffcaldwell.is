a30e7ebef50a9829c7f9181a0217fcff3c9fee76
nemo-omen <nemo.omen@gmail.com>
2024-02-06 13:37:20 -0600

    Futz around with formatted date component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeffcaldwell.is',
+  integrations: [mdx(), lit()]
 });
---
+++ src/components/content/creating-a-markdown-editor/markdown-editor.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit';
+
+export class MarkdownEditor extends LitElement {
+  render() {
+    return html`<p>DOOOOKIE</p>`;
+  }
+}
+
+customElements.define('markdown-editor', MarkdownEditor);
